54ce69a23c316bb0cf369aab7aae6a434c4190e5
Viet Nguyen <vietndph29972@fpt.edu.vn>
2023-12-17 01:13:52 +0700

    a

--- Resort_Demo_Project/obj/Debug/net6.0/Resort_Demo_Project.AssemblyInfo.cs
+++ Resort_Demo_Project/obj/Debug/net6.0/Resort_Demo_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Resort_Demo_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07c1bbe67c10abd6d2e233e55cb943fd8d62397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Resort_Demo_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Resort_Demo_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
