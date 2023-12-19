16cb6163dce755c929d10f32d99441a786371879
Tbinh1505 <phungthanhbinh594@gmail.com>
2023-12-19 21:59:51 +0700

    phanQuyen

---
+++ Resort_Demo_Project/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resort_Demo_Project.Areas.Admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        public IActionResult IndexAdmin()
+        {
+            return View();
+        }
+    }
+}
